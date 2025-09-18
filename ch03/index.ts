47fc1652674c722a87033d4d632180a365403e38
aamoos <aamoos@openmate.co.kr>
2025-09-18 11:27:32 +0900

    타입스크립트 - 객체 타입의 유니언 Union

--- ch03/index.ts
+++ ch03/index.ts
@@ -143,3 +143,64 @@ type Poem = {
     author: Author;
     name: string;
 }
+
+// 선택적 속성과 undefined를 포함한 유니언 타입의 속성 사이에는 차이가 있음을 명심하세요.
+// ?를 사용해 선택적으로 선언된 속성은 존재하지 않아도 됩니다.
+// 그러나 필수로 선언된 속성과 | undefined는 그 값이 undefined 일지라도 반드시 존재해야 합니다.
+
+// 3. 객체 타입 유니언
+// 타입스크립트 코드에서는 속성이 조금 다른, 하나 이상의 서로 다른 객체 타입이 될 수 있는 타입을 설명할 수 있어야 합니다.
+// 또한, 속성값을 기반으로 해당 객체 타입 간에 타입을 좁혀야 할 수도 있습니다.
+
+// 3.1 유츄된 객체 타입 유니언
+// 변수에 여러 객체 타입 중 하나가 될 수 있는 초깃값이 주어지면 타입스크립트는 해당 타입을 객체 타입 유니언으로 유추합니다.
+// 유니언 타입은 가능한 각 객체 타입을 구성하고 있는 요소를 모두 가질 수 있습니다.
+
+
+const book2: Book2 = 
+Math.random() > 0.5 
+? { 
+    name: "개발자 9Diin"
+    , pages: 80 
+} 
+: { 
+    name: "개발자 9Diin"
+    , rhymes: true 
+};
+
+book2.name;     // 타입 : string
+book2.pages;    // 타입 : number | undefined
+book2.rhymes;   // 타입 boolean | undefiend
+
+// 이 경우 타입은 어떻게 될까요?
+// 아래 선언한 BookWithPages와 BookWithRhymes 둘 중 하나가 되어야만 한다.
+
+type BookWithPages = {
+    name: string;
+    pages: number;
+};
+type BookWithRhymes = {
+    name: string;
+    rhymes: boolean;
+};
+type Book2 = BookWithPages | BookWithRhymes;
+
+// 3.2 교차 타입
+// 타입스크립트 유니언 타입은 둘 이상의 다른 타입 중 하나의 타입이 될 수있음을 나타냅니다.
+
+type Artwork = {
+    genre: string;
+    name: string;
+}
+
+type Writing = {
+    pages: number;
+    name: string;
+}
+
+type WriitenArt = Artwork & Writing;
+// type WriitenArt = {
+//     genre: string;
+//     name: string;
+//     pages: number;
+// }
